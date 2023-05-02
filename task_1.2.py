44c5861583bc603d8eb90aa98a06e1c85cbfe021
Ole4kabelousova <129664704+Ole4kabelousova@users.noreply.github.com>
2023-05-02 10:06:49 +0300

    Add files via upload

---
+++ task_1.2.py
@@ -0,0 +1,75 @@
+# Задача 1.2.
+
+# Пункт A. 
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+import random
+x = my_favorite_songs
+nev_songs = random.sample(x,3)
+total_nev_songs = 0
+for i in nev_songs:
+  total_nev_songs += i[1]
+print(f'Три песни звучат {total_nev_songs} минут')
+
+
+# Пункт B. 
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+from random import sample
+x = list(my_favorite_songs_dict.items())
+nev_songs = sample(x,3)
+# print(nev_songs)
+total_nev_songs = 0
+for i in nev_songs:
+  total_nev_songs += i[1]
+print(f'Три песни звучат {total_nev_songs} минут')
+
+# Дополнительно для пунктов A и B
+# Пункт C.
+# Сгенерируйте случайные песни с помощью модуля random
+# import random
+
+import random
+
+print(random.choice(my_favorite_songs))
+
+print(random.choice(list(my_favorite_songs_dict.items())))
+
+
+# Дополнительно 
+# Пункт D.
+# Переведите минуты и секунды в формат времени. Используйте модуль datetime 
+
+import datetime
+time = total_nev_songs
+res = datetime.timedelta(time)
+print(res)
